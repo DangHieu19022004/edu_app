f7ea844a92c6957f098eac94b0349503bd2da0a1
DangHieu19022004 <danghieu19122004@gmail.com>
2026-03-09 13:11:31 +0700

    setup swagger

--- be/config/settings.py
+++ be/config/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     "corsheaders",
     # Local apps
     "core",
+    # swagger
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- be/core/urls.py
+++ be/core/urls.py
@@ -1,4 +1,7 @@
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -8,7 +11,18 @@ router = DefaultRouter()
 # Register your viewsets here
 # router.register(r'example', ExampleViewSet)
 
+# Define schema view
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('', include(router.urls)),
     path('health/', views.health_check, name='health-check'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
