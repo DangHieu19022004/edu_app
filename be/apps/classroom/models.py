85c2bddf696635bab858ee8fee65aa2053370f46
DangHieu19022004 <danghieu19122004@gmail.com>
2026-03-14 16:50:24 +0700

    setup classroom

--- be/apps/classroom/models.py
+++ be/apps/classroom/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+import uuid
+from mongoengine import Document, StringField
 
-# Create your models here.
+
+class Class(Document):
+    id = StringField(primary_key=True, default=lambda: str(uuid.uuid4()), max_length=36)
+    name = StringField(required=True, max_length=100)  # VD: 10A1
+    teacher_id = StringField(required=True, max_length=100)  # Liên kết đến bảng Teachers
+    school_name = StringField(required=True, max_length=255)
+    class_year = StringField(required=True, max_length=50)  # VD: K64, 2022-2025
+
+    meta = {
+        "collection": "classes",
+        "indexes": ["teacher_id", "class_year", "school_name"],
+    }
+
+    def __str__(self):
+        return f"{self.name} - {self.class_year}"
